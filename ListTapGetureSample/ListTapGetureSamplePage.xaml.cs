5f6f2635cff307b4b86b53ad06be305e3f2b8e18
Gerald Versluis <gerald@verslu.is>
2017-08-29 11:24:09 +0200

    Initial commit
    Code for the answer on this [SO
    question](https://stackoverflow.com/questions/45934764/how-to-handle-eve
    nt-for-a-specific-control-in-listviewxamarin)

---
+++ ListTapGetureSample/ListTapGetureSamplePage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace ListTapGetureSample
+{
+    public class Foo 
+    {
+        public string Bar { get; set; }
+    }
+
+    public partial class ListTapGetureSamplePage : ContentPage
+    {
+        public ObservableCollection<Foo> Items { get; set; } = new ObservableCollection<Foo>();
+
+        public Command TapCommand => new Command<Foo>((fooObject) => Tapped(fooObject));
+
+        private void Tapped(Foo fooObject)
+        {
+            DisplayAlert("Tapped!", fooObject.Bar, "Gotcha");
+        }
+
+        public ListTapGetureSamplePage()
+        {
+            InitializeComponent();
+
+            Items.Add(new Foo
+            {
+                Bar = "1"
+            });
+
+			Items.Add(new Foo
+			{
+				Bar = "2"
+			});
+
+			Items.Add(new Foo
+			{
+				Bar = "3"
+			});
+
+			Items.Add(new Foo
+			{
+				Bar = "4"
+			});
+
+			Items.Add(new Foo
+			{
+				Bar = "5"
+			});
+
+            BindingContext = this;
+        }
+    }
+}
